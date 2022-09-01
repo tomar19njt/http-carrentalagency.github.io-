d17ceb1a80d0c74a6853daec67e13599e0bbaf2e
Nidhi Rathor <110535931+tomar19njt@users.noreply.github.com>
2022-09-01 19:47:57 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3 col-md-2">
-          <div class="logo"> <a href="index.php"><img src="assets/images/logo1.png" alt="image"/></a> </div>
+          <div class="logo"> <a href="index.html"><img src="assets/images/logo1.png" alt="image"/></a> </div>
         </div>
         <div class="col-sm-9 col-md-10">
           <div class="header_info">
@@ -79,7 +79,7 @@ foreach($results as $result)
       </div>
       <div class="collapse navbar-collapse" id="navigation">
         <ul class="nav navbar-nav">
-          <li><a href="index.php">Home</a>    </li> 	 
+          <li><a href="index.html">Home</a>    </li> 	 
           <li><a href="page.php?type=aboutus">About Us</a></li>
           <li><a href="car-listing.php">Car Listing</a>
           <li><a href="contact-us.php">Contact Us</a></li>
