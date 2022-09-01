302d538c4c713803382275748ce0fb221c92a56f
Nidhi Rathor <110535931+tomar19njt@users.noreply.github.com>
2022-09-01 19:55:49 +0530

    Update edit-vehicle.php

--- admin/edit-vehicle.php
+++ admin/edit-vehicle.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(0);
 include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
-	header('location:index.php');
+	header('location:index.html');
 } else {
 
 	if (isset($_POST['submit'])) {
